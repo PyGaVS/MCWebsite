ddc892329f54e9f9e5b7c9c32ee21e4273c6e3ea
Lylian <lylian270104@gmail.com>
2023-07-21 21:12:20 +0200

    commit

--- config/env.php
+++ config/env.php
@@ -5,19 +5,7 @@ $ENV =[
     'APP_VERSION' => 'v0',
     // Paramètres pour l'accès à la base de données
     'DB_HOST'     => 'localhost',
-    'DB_DATABASE' => 'MCMobRanker_v0',
+    'DB_DATABASE' => '',
     'DB_USERNAME' => 'PyGaVS',
-    'DB_PASSWORD' => 'ibxh328*gi73tytX'
+    'DB_PASSWORD' => ''
 ];
-
-/*
-$ENV =[
-    // Paramètres pour l'application
-    'APP_NAME'    => 'MCWebsite',
-    'APP_VERSION' => 'v0',
-    // Paramètres pour l'accès à la base de données
-    'DB_HOST'     => 'sql108.infinityfree.com',
-    'DB_DATABASE' => 'if0_34612863_MCMobRanker_v0',
-    'DB_USERNAME' => 'if0_34612863',
-    'DB_PASSWORD' => 'ngnWsZuiby'
-];*/
